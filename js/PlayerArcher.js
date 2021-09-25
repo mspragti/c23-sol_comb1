339a218bdae58e701361c65bb33d9f5ab87178e4
mspragti <83079149+mspragti@users.noreply.github.com>
2021-09-25 20:02:26 +0530

    Update PlayerArcher.js

--- js/PlayerArcher.js
+++ js/PlayerArcher.js
@@ -17,7 +17,12 @@ class PlayerArcher {
   display() {
     var pos = this.body.position;
     var angle = this.body.angle;
-
+ if (keyIsDown(RIGHT_ARROW)&&this.angle<70) {
+      this.angle+=1
+    }
+    if (keyIsDown(LEFT_ARROW)&&this.angle>-30) {
+      this.angle-=1
+    }
     
     
 
